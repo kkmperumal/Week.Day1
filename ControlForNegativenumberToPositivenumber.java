7d11c35476392ab90c56f7efc78e5119055f1cdc
KakkumPerumal <66548750+kkmperumal@users.noreply.github.com>
2021-09-05 05:47:53 +0530

    Add files via upload

---
+++ ControlForNegativenumberToPositivenumber.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class ControlForNegativenumberToPositivenumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = -10;
+		if (i < 0) {
+			System.out.println("Positive of " + i + " is " + (-i));
+		} else {
+			System.out.println(i + " is Positive Number only");
+		}
+	}
+
+}
---
+++ ControlIfPositiveOrNegative.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class ControlIfPositiveOrNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 2;
+		if (i > 0) {
+			System.out.println("The number is Positive");
+		} else if (i < 0) {
+			System.out.println("The number is Negative");
+		} else {
+			System.out.println("The number is neither Positive nor Negative");
+		}
+	}
+
+}
